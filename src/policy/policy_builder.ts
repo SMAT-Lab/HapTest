164db6bc28fb2b93cfd9ab6a4ef29177ac56118c
code-sunbo <bb.sunbo@huawei.com>
2024-07-24 16:38:26 +0800

    rename
    Signed-off-by: code-sunbo <bb.sunbo@huawei.com>

--- src/policy/policy_builder.ts
+++ src/policy/policy_builder.ts
@@ -18,8 +18,8 @@ import { Hap } from '../model/hap';
 import { FuzzOptions } from '../runner/fuzz_options';
 import { InputPolicy, PolicyName } from './input_policy';
 import { ManualPolicy } from './manual_policy';
-import { UtgGreedySearchPolicy } from './ugt_greedy_search_policy';
-import { UtgNaiveSearchPolicy } from './ugt_naive_search_policy';
+import { UtgGreedySearchPolicy } from './utg_greedy_search_policy';
+import { UtgNaiveSearchPolicy } from './utg_naive_search_policy';
 
 export class PolicyBuilder {
     static buildPolicyByName(device: Device, hap: Hap, options: FuzzOptions): InputPolicy {
